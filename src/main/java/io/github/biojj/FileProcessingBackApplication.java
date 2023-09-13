a75f5f7c39b35e8f7ae1e1f24fef028ca4dd9091
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-13 12:19:48 -0400

    feat:  ✨ init

---
+++ src/main/java/io/github/biojj/FileProcessingBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.biojj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileProcessingBackApplication.class, args);
+    }
+
+}
